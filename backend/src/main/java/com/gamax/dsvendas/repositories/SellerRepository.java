7f302304ef33ca33b19cca5d314534bb798574b4
Alex Maximiano <n4ow.dev@gmail.com>
2021-11-03 09:55:30 -0300

    Layers

---
+++ backend/src/main/java/com/gamax/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.gamax.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gamax.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
